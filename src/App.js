b02ebf5e05bbc8822edb69a7f3d5675f670961da
Your Name <you@example.com>
2022-03-21 12:29:16 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import MyNav from './components/MyNav/MyNav';
+import MyForm from './components/MyForm/MyForm';
+
+function App() {
+  return (
+    <div className="App">
+<MyNav/>
+<MyForm/>
+    </div>
+   
+  );
+}
+
+export default App;
